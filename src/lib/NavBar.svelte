68ba1c313f9a8376ace6e87fe38de1fa5598a7dc
Charles Alaras <charles.alaras@gmail.com>
2023-01-08 15:31:54 -0800

    Add PostCSS + NavBar beginnings

---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,15 @@
+<script>
+    let page = "home"
+    let hovered = false
+</script>
+
+<div class="navbar">
+    
+</div>
+
+<style>
+    .navbar {
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        backdrop-filter: blur(15px);
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import NavBar from '../lib/NavBar.svelte';
+</script>
+
+<div class="app">
+    <NavBar />
+
+    <main>
+        <slot />
+    </main>
+</div>
