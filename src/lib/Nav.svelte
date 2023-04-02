592cf4cbea1b78d1437abbcb409af325266672e1
Charles Alaras <charles.alaras@gmail.com>
2023-04-01 22:39:07 -0700

    Update file structure + transitions

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { goto } from '$app/navigation';
+    let hovered = false
+	
+</script>
+
+<div class="navbar">
+    <div on:click={() => { goto('/') }}>INTRO</div>
+    <div on:click={() => { goto('/about') }}>ABOUT</div>
+    <div on:click={() => { goto('/projects') }}>PROJECTS</div>
+    <div on:click={() => { goto('/contact') }}>CONTACT</div>
+    <div on:click={() => { goto('/blog') }}>BLOG</div>
+</div>
+
+<style>
+    .navbar {
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        backdrop-filter: blur(15px);
+    }
+</style>
---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { fade } from 'svelte/transition'
+	import Nav from '../../lib/Nav.svelte'
+	
+	export let data
+</script>
+
+<Nav />
+{#key data.pathname}
+<div class="app"
+	in:fade={{ duration: 300, delay: 400 }}
+	out:fade={{ duration: 300 }}
+>
+
+    <main>
+        <slot />
+    </main>
+</div>
+{/key}
