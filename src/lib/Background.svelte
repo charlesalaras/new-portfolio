b2442e48afe3b0657bfab920d06e2a09e72e9ec8
Charles Alaras <charles.alaras@gmail.com>
2023-05-11 16:04:48 -0700

    Add resizing reactivity

--- src/lib/Background.svelte
+++ src/lib/Background.svelte
@@ -10,17 +10,30 @@
 		'#121212', // black
 	];
 
-	onMount(() => {
-        canvasElement.width = width;
-        canvasElement.height = height;
-		const ctx = canvasElement.getContext("2d");
-		
+    function draw(ctx) {
         ctx.fillStyle = "rgb(200, 0, 0)";
         ctx.fillRect(10, 10, 50, 50);
 
         ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
         ctx.fillRect(30, 30, 50, 50);
+
+    }
+    
+	onMount(() => {
+        canvasElement.width = width;
+    	canvasElement.height = height;
+		const ctx = canvasElement.getContext("2d");
+        draw(ctx);
 	})
+
+    $: {
+        if(canvasElement) {
+			canvasElement.width = width;
+			canvasElement.height = height;
+			const ctx = canvasElement.getContext("2d");
+			draw(ctx);
+        }
+    }
 </script>
 
 <svelte:window bind:innerWidth={width} bind:innerHeight={height}/>
@@ -35,5 +48,6 @@ canvas {
     left: 0px;
     width: 100vw;
     height: 100vh;
+	z-index: 2;
 }
 </style>
