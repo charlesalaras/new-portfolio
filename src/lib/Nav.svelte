c36aee65c5247cdfd881d8e2598675ddf0c28388
Charles Alaras <charles.alaras@gmail.com>
2023-05-27 16:33:47 -0700

    Add shard animation

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -14,5 +14,54 @@ export let pathname;
 	.bar {
 		border-radius: calc(2rem / 3);
 		gap: 2rem;
+		box-sizing: border-box;
+	}
+	.bar:after {
+		content: "";
+		height: 1px;
+		width: 20%;
+		position: absolute;
+		top: 100%;
+		transition: left, opacity;
+		transition-duration: 600ms;
+		transition-timing-function: ease;
+		background: linear-gradient(90deg,
+			transparent,
+			blue,
+			transparent
+		);
+		opacity: 0;
+	}
+	.bar:after {
+		left: 65%;
+		opacity: 0;
+	}
+	.bar:hover:after {
+		left: 15%;
+		opacity: 1;
+	}
+	.bar:before {
+		content: "";
+		height: 1px;
+		width: 20%;
+		position: absolute;
+		top: -1px;
+		transition: left, opacity;
+		transition-duration: 600ms;
+		transition-timing-function: ease;
+		background: linear-gradient(90deg,
+			transparent,
+			blue,
+			transparent
+		);
+		opacity: 0;
+	}
+	.bar:before {
+		left: 15%;
+		opacity: 0;
+	}
+	.bar:hover:before {
+		left: 65%;
+		opacity: 1;
 	}
 </style>
