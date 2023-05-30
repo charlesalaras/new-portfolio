82bd833c65a88817592dcd03cb60cac1eac652ed
Charles Alaras <charles.alaras@gmail.com>
2023-05-29 21:13:49 -0700

    Prepare navbar for animation

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -3,14 +3,50 @@ export let pathname;
 </script>
 
 <div class="bar hc">
-    <a href='/'>INTRO</a>
-    <a href='/about'>ABOUT</a>
-    <a href='/projects'>PROJECTS</a>
-    <a href='/contact'>CONTACT</a>
-    <a href='/blog'>BLOG</a>
+<ul>
+    <li>
+		<a href='/'>INTRO</a>
+	</li>
+    <li>
+		<a href='/about'>ABOUT</a>
+	</li>
+    <li>
+		<a href='/projects'>PROJECTS</a>
+	</li>
+    <li>
+		<a href='/contact'>CONTACT</a>
+	</li>
+    <li>
+		<a href='/blog'>BLOG<div class="icon bi-box-arrow-up-right"></div></a>
+	</li>
+</ul>
+<div class="underbar"></div>
 </div>
 
 <style>
+	ul {
+		list-style: none;
+		padding: 0;
+	}
+	.bi-box-arrow-up-right {
+		mask: url("box-arrow-up-right.svg") no-repeat 50% 50%;
+	}
+	.icon {
+		width: 1rem;
+		height: 1rem;
+	}
+	.underbar {
+		transition: width;
+		transition-duration: 500ms;
+		transition-timing-function: ease;
+	}
+	.bar .underbar {
+		width: 50%;
+		height: 0.1rem;
+	}
+	.bar:hover .underbar {
+		width: 100%;
+	}
 	.bar {
 		border-radius: calc(2rem / 3);
 		gap: 2rem;
