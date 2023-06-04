ac9a0fc2a13e8d2454ca017ef9e08fa2ea4f0209
Charles Alaras <charles.alaras@gmail.com>
2023-06-04 15:16:45 -0700

    Update Blog layout

---
+++ src/lib/BlogPost.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Chip from './Chip.svelte'
+
+	export let postTitle;
+	export let byline;
+	export let date;
+	export let tags = [];
+</script>
+
+<div class="post">
+	<h1 id="post-title">{postTitle}</h1>
+	<p id="post-byline">{byline}</p>
+	<p id="post-date">Posted on {date}</p>
+	<div class="tags">
+		{#each tags as tag}
+			<Chip icon={tag.icon} content={tag.content}/>
+		{/each}
+	</div>
+</div>
+
+<style>
+.post {
+	line-height: 0.5;
+	padding: 0.5rem 1rem;
+	width: 100%;
+	border-radius: 14px;
+	background-color: rgba(255, 255, 255, 0.5);
+}
+h1 {
+	font-size: 1.25rem;
+}
+.tags {
+	height: 1.5rem;
+}
+</style>
---
+++ src/lib/Chip.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let icon;
+	export let content;
+</script>
+
+<div>
+{icon} {content}
+</div>
+
