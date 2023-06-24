9cfdb13cfb83cdcce90f3f348047d679193a940b
Charles Alaras <charles.alaras@gmail.com>
2023-06-23 21:13:08 -0700

    Add markup for about

---
+++ src/lib/About.svelte
@@ -0,0 +1,20 @@
+<div class="content">
+	<div class="hexagon education"></div>
+	<div class="hexagon github"></div>
+	<div class="hexagon linkedin"></div>
+	<div class="hexagon skills"></div>
+	<div class="hexagon past-work"></div>
+</div>
+
+<style>
+.content {
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.hexagon {
+
+}
+</style>
