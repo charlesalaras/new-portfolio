ae367b1f3aa88d523484fb30c8cd09996cd258b4
Charles Alaras <charles.alaras@gmail.com>
2023-08-28 22:02:00 -0700

    Add swup + compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,9 @@
-export default {}
+import { defineConfig } from "astro/config";
+import compressor from "astro-compressor";
+
+import swup from "@swup/astro";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [swup(), compressor()]
+});
